17e6f9f9393ec4fd9ccf185933689e900985b9e0
JeffHenrique10 <jeffhenrique91@gmail.com>
2019-05-08 16:12:28 -0300

    Desafio

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const app = express()
+nunjucks.configure('view', {
+  autoscape: true,
+  express: app,
+  watch: true
+})
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
+const logMiddleware = (req, res, next) => {
+  console.log(
+    `Host: ${req.headers.host} | URL: ${req.url} METHOD: ${req.method}`
+  )
+  const idade = req.query
+  if (!idade) {
+    res.redirect('/')
+  }
+  return next()
+}
+app.use(logMiddleware)
+
+app.get('/', (req, res) => res.render('inicio'))
+app.get('/maior', logMiddleware, (req, res) => {
+  const { idade } = req.query
+  const { nome } = req.query
+  res.render('maior', { idade, nome })
+})
+app.get('/menor', logMiddleware, (req, res) => {
+  const { idade } = req.query
+  const { nome } = req.query
+  res.render('menor', { idade, nome })
+})
+app.post('/check', (req, res) => {
+  const { idade } = req.body
+  const { nome } = req.body
+  if (idade >= 18) {
+    res.redirect(`/maior?nome=${nome}&idade=${idade}`)
+  } else {
+    res.redirect(`/menor?nome=${nome}&idade=${idade}`)
+  }
+})
+
+app.listen(3000)
